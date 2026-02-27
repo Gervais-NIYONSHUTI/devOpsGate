f9b36d3ddf1c49e524c905361ef1df6e870ba3f1
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2026-02-27 08:48:55 +0200

    Change browser launch to headless mode in tests

--- src/test/java/Tests/SortingTest.java
+++ src/test/java/Tests/SortingTest.java
@@ -17,7 +17,7 @@ public class SortingTest {
     @BeforeAll
     static void setup() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch( new BrowserType.LaunchOptions().setHeadless(false) );
+        browser = playwright.chromium().launch( new BrowserType.LaunchOptions().setHeadless(true) );
     }
 
     @AfterAll
